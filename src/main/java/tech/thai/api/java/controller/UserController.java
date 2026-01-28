0dba776ffe6e60cb641532bba63846c85bb7da55
ThaigoDev <thaigodeveloper@gmail.com>
2026-01-28 16:52:48 -0300

    chore: config do ambiente

---
+++ src/main/java/tech/thai/api/java/controller/UserController.java
@@ -0,0 +1,17 @@
+package tech.thai.api.java.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody String body) {
+        return null;
+    }
+}
